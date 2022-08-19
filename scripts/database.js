ab7f26dcdee2ef3e9966b09c2013453b828743ea
Kunal Mali <kunalmali299@gmail.com>
2022-08-19 19:01:09 +0530

    Secured the whole project

--- scripts/database.js
+++ scripts/database.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const { createPool } = require('mysql');
 
 const pool = createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"interwoven_db",
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
     connectionLimit:10
 })
 
@@ -14,3 +15,5 @@ pool.query(`select * from sign_up_details`, (err, result, fields)=>{
     } 
     return console.log(result);
 });
+
+module.exports = pool;
